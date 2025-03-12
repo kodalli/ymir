0d55de4632bebccf34ada058e76725e4236f232c
kodalli <surya.kodali@gmail.com>
2025-03-12 17:17:02 -0400

    style: Update color palette to use custom 'ymir' colors across templates and stylesheets

---
+++ ymir/static/js/tailwind-config.js
@@ -0,0 +1,22 @@
+// Ymir custom Tailwind configuration
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'ymir': {
+                    50: '#fdf2f4',
+                    100: '#fce7ea',
+                    200: '#f9d0d6',
+                    300: '#f4a9b5',
+                    400: '#ed758a',
+                    500: '#e93b52', // Main color
+                    600: '#d92d44',
+                    700: '#b62238',
+                    800: '#961f32',
+                    900: '#7c1f2e',
+                    950: '#440c14',
+                }
+            }
+        }
+    }
+};
